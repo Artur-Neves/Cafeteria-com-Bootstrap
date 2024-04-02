8781f51eba90e0993171dacb7d83fdba38f1d612
Artur_Linux <nevesdev.ti@gmail.com>
2024-04-02 15:20:13 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const formulario = document.getElementById("Contato");
 const divFormulario = document.querySelector(".formulario-entre_em_contato__div");
-
+const btnModoNoturno = document.getElementById("flexSwitchCheckModoEscuro");
+const body = document.querySelector("body");
 formulario.addEventListener("submit", (evento)=>{
     if (!formulario.checkValidity()){
         evento.preventDefault();
@@ -8,3 +9,7 @@ formulario.addEventListener("submit", (evento)=>{
         divFormulario.style.height = "700px";
     }
 })
+btnModoNoturno.addEventListener("click", ()=>{
+    const modo = (btnModoNoturno.checked ? 'dark' : 'light');
+        body.setAttribute("data-bs-theme", modo);
+});
